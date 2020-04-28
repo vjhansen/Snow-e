01b89c2b5875cdc47b478ea9c54739cd24d8ca79
[Deivy] <deivydas.kazokas@gmail.com>
2020-04-28 23:39:33 +0200

    test sync

--- Scenario_1/controllers/zig_zag_GPS/build/release/zig_zag_GPS.d
+++ Scenario_1/controllers/zig_zag_GPS/build/release/zig_zag_GPS.d
@@ -1,9 +1,9 @@
 build/release/zig_zag_GPS.o: zig_zag_GPS.c \
-  /Applications/Webots.app/include/controller/c/webots/motor.h \
-  /Applications/Webots.app/include/controller/c/webots/types.h \
-  /Applications/Webots.app/include/controller/c/webots/robot.h \
-  /Applications/Webots.app/include/controller/c/webots/nodes.h \
-  /Applications/Webots.app/include/controller/c/webots/distance_sensor.h \
-  /Applications/Webots.app/include/controller/c/webots/compass.h \
-  /Applications/Webots.app/include/controller/c/webots/keyboard.h \
-  /Applications/Webots.app/include/controller/c/webots/gps.h
+ D:\Webots\Webots/include/controller/c/webots/motor.h \
+ D:\Webots\Webots/include/controller/c/webots/types.h \
+ D:\Webots\Webots/include/controller/c/webots/robot.h \
+ D:\Webots\Webots/include/controller/c/webots/nodes.h \
+ D:\Webots\Webots/include/controller/c/webots/distance_sensor.h \
+ D:\Webots\Webots/include/controller/c/webots/compass.h \
+ D:\Webots\Webots/include/controller/c/webots/keyboard.h \
+ D:\Webots\Webots/include/controller/c/webots/gps.h
