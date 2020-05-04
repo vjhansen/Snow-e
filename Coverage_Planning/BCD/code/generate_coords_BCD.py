bf4066d1a9d0e147a1e2ec43eef5c6f134009a11
Victor Hansen <0vjhansen@gmail.com>
2020-05-04 10:36:28 +0200

    update

--- Coverage_Planning/BCD/code/generate_coords_BCD.py
+++ Coverage_Planning/BCD/code/generate_coords_BCD.py
@@ -2,35 +2,10 @@
 
 # Snow-e
 # Engineer: V. J. Hansen
-# 03.05.2020
+# 04.05.2020
 
 import math
 import csv
-
-size_x = 10  # height
-size_z = 20  # width
-delta = 0.5
-
-num_points = 10
-
-def generate_x(num_points):
-    x = {}
-    for n in range(num_points):
-        x[n] = (-size_x/2.0) * ((1 + 3*n + n**2 + 2*(n**3)) - 4*(math.floor(0.25 * (2 + 3*n + (n**2) + 2*(n**3)))))
-    return x
-
-
-def generate_z(num_points):
-    z = {}
-    for n in range (num_points):
-        z[n] = (-size_z/2.0) + delta*math.ceil(n/2)
-    return z
-
-
-target_points = 4*(size_z) # same as 2*(size_z//delta)
-a = generate_x(target_points)
-b = generate_z(target_points)
-
 
 with open('coordinates.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
@@ -48,9 +23,39 @@ with open('coordinates.csv') as csvfile:
         x_s_coord.append(x_start)
         x_e_coord.append(x_end)
 
-print(z_s_coord[0])
-print(z_e_coord[0])
+#print(z_s_coord[0],z_s_coord[1],z_s_coord[2],z_s_coord[3],z_s_coord[4])
+#print(z_e_coord[0],z_e_coord[1],z_e_coord[2],z_e_coord[3],z_e_coord[4])
+
+#print(x_s_coord[0],x_s_coord[1],x_s_coord[2],x_s_coord[3],x_s_coord[4])
+#print(x_e_coord[0],x_e_coord[1],x_e_coord[2],x_e_coord[3],x_e_coord[4])
 
 
-print(x_s_coord[0])
-print(x_e_coord[0])
+size_x = abs(float(x_e_coord[2])-float(x_s_coord[2])) # height
+size_z = abs(float(z_s_coord[2])-float(z_e_coord[2])) # width
+print(size_x)
+print(size_z)
+#size_x = 10  
+#size_z = 20  
+delta = 0.5
+
+
+def generate_x(num_points):
+    x_coord = []
+    for n in range(num_points):
+        x = (-size_x/2.0) * ((1 + 3*n + n**2 + 2*(n**3)) - 4*(math.floor(0.25 * (2 + 3*n + (n**2) + 2*(n**3)))))
+        x_coord.append(x)
+    return x_coord
+
+def generate_z(num_points):
+    z_coord = []
+    for n in range (num_points):
+        z = (-size_z/2.0) + delta*math.ceil(n/2)
+        z_coord.append(z)
+    return z_coord
+
+target_points = 4*(int(size_z)) # same as 2*(size_z//delta=0.5)
+a = generate_x(target_points)
+b = generate_z(target_points)
+
+print(a)
+print(b)
