607c519d01ed96ee38c924af82b08d1f28e5cb8d
Victor Hansen <46825640+vjhansen@users.noreply.github.com>
2020-05-06 15:56:15 +0200

    Update image_to_binary_map.py

--- Coverage_Planning/BCD/code/image_to_binary_map.py
+++ Coverage_Planning/BCD/code/image_to_binary_map.py
@@ -1,16 +1,14 @@
 # Create binary map from image
 
 # V. J. Hansen
-# 03.05.2020
+# 06.05.2020
 
 # https://stackoverflow.com/questions/50234485/drawing-rectangle-in-opencv-python/50235566
 # https://stackoverflow.com/questions/50368683/set-white-color-outside-boundingbox-python-opencv
 
 ## python3 image_to_binary_map.py --image sim_lot.png
 
-import cv2
-import argparse
-import os
+import cv2, argparse, os
 import numpy as np
 
 # initialize the list of reference point
@@ -20,18 +18,16 @@ def shape_selection(event, x, y, flags, param):
     # grab references to the global variables
     global ref_point, crop
 
-    # if the left mouse button was clicked, record the starting
-    # (x, y) coordinates and indicate that cropping is being performed
+    # if left mouse button was clicked, record the starting (x, y) coordinates and indicate that cropping is being performed
     if event == cv2.EVENT_LBUTTONDOWN:
         ref_point = [(x, y)]
 
-    # check to see if the left mouse button was released
+    # check if left mouse button was released
     elif event == cv2.EVENT_LBUTTONUP:
-        # record the ending (x, y) coordinates and indicate that
-        # the cropping operation is finished
+        # record the ending (x, y) coordinates and indicate that the cropping operation is finished
         ref_point.append((x, y))
 
-        # draw a rectangle around the region of interest
+        # draw a rectangle around the region of interest (roi)
         x1, y1 = ref_point[0]
         x2, y2 = ref_point[1]
         roi = image[y1:y2, x1:x2]
@@ -44,7 +40,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
 
-# load the image, clone it, and setup the mouse callback function
+# load image, clone it, and initialize the mouse callback function
 image = cv2.imread(args["image"])
 white_bg = 255*np.ones_like(image)
 clone = image.copy()
@@ -57,23 +53,22 @@ print("Press 'r' to reset")
 print("Press 'v' to view binary map")
 print("Press 'q' to save image and quit")
 
-# keep looping until the 'q' key is pressed
+# keep looping until 'q'-key is pressed
 while True:
-    # display the image and wait for a keypress
+    # display image and wait for a keypress
     cv2.imshow("image", image)
-    
     key = cv2.waitKey(1) & 0xFF
 
     # press 'r' to reset the window
     if key == ord("r"):
         image = clone.copy()
-
+    # press 'v' to view binary map
     if key == ord("v"):
         cv2.imshow('binary map', white_bg)
-
     # press 'q' to quit
     elif key == ord("q"):
-        resized_img = cv2.resize(white_bg, (251, 221))
+        resized_img = cv2.resize(white_bg, (251, 221)) # resize image
+        # add black border to resized image (the border is needed for the BCD to set the boundary of our area)
         bordersize = 1
         border = cv2.copyMakeBorder(
                         resized_img,
