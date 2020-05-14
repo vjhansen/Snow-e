334967492fc62594154728645810d8bb94abb8ef
Victor Hansen <0vjhansen@gmail.com>
2020-05-14 09:26:57 +0200

    update main_BCD.py

--- Simulation/Scenario_2/Coverage_Planning/main_BCD.py
+++ Simulation/Scenario_2/Coverage_Planning/main_BCD.py
@@ -3,20 +3,155 @@
 #-----------------------------------------------
 # Apply Boustrophedon Cellular Decomposition to a map
 # Engineer(s): V. J. Hansen
-# Version 0.8.0
-# Data: 13.05.2020
+# Version 0.9.1
+# Data: 14.05.2020
 
 #-----------------------------------------------
 from matplotlib import pyplot as plt
-import bcd  # Boustrophedon Cellular decomposition
-import cv2, csv, os
+from typing import Tuple, List
+import csv, os, random, itertools
+import cv2
 import numpy as np
 #-----------------------------------------------
+
+Slice = List[Tuple[int, int]]
+
+#-------------------------------------
+def calc_connectivity(slice: np.ndarray) -> Tuple[int, Slice]:
+    """ Calculates the connectivity of a slice and returns the connected area of ​​the slice.
+    Args:       slice: rows. A slice of map.
+
+    Returns:    The connectivity number and connectivity parts.
+                connectivity --> total number of connected parts """
+    connectivity = 0
+    last_data = 0
+    open_part = False
+    connective_parts = []
+    for i, data in enumerate(slice):
+        if last_data == 0 and data == 1:
+            open_part = True
+            start_point = i
+        elif last_data == 1 and data == 0 and open_part:
+            open_part = False
+            connectivity += 1
+            end_point = i
+            connective_parts.append((start_point, end_point))
+        last_data = data
+    return connectivity, connective_parts
+
+#-------------------------------------
+def get_adjacency_matrix(parts_left: Slice, parts_right: Slice) -> np.ndarray:
+    """ Get adjacency matrix of 2 neighborhood slices.
+    Args:       slice_left: left slice, slice_right: right slice """
+    adjacency_matrix = np.zeros( [len(parts_left), len(parts_right)] )
+    for l, lparts in enumerate(parts_left):
+        for r, rparts in enumerate(parts_right):
+            if min(lparts[1], rparts[1]) - max(lparts[0], rparts[0]) > 0:
+                adjacency_matrix[l, r] = 1
+    return adjacency_matrix     # [L, R] Adjacency matrix.
+
+#-------------------------------------
+def remove_duplicates(in_list):
+    """ This function removes duplicates in the input list, where
+        input list is composed of unhashable elements """
+    out_list = []
+    in_list.sort()
+    out_list = list(in_list for in_list, _ in itertools.groupby(in_list))
+    return out_list
+
+#-------------------------------------
+def bcd(erode_img: np.ndarray) -> Tuple[np.ndarray, int]:
+    """ Boustrophedon Cellular Decomposition
+    Args:   erode_img: [H, W], eroded map. pixel value 0 represents obstacles, 1 = free space.
+    Returns:
+        [H, W], separated map. pixel value 0 represents obstacles for its cell number.
+        current_cell and seperate_img is for display purposes --> which is used to show
+        decomposed cells into a separate figure
+        all_cell_numbers --> contains all cell index numbers
+        cell_boundaries --> contains all cell boundary coordinates (only y coordinate)
+        non_neighboor_cells --> contains cell index numbers of non_neighboor_cells, i.e.
+        cells which are separated by the objects """
+    last_connectivity = 0
+    last_connectivity_parts = []
+    current_cell = 1
+    current_cells = []
+    separate_img = np.copy(erode_img)
+    cell_boundaries = {}
+    non_neighboor_cells = []
+
+    for col in range(erode_img.shape[1]):
+        current_slice = erode_img[:, col]
+        connectivity, connective_parts = calc_connectivity(current_slice)
+        if last_connectivity == 0:
+            current_cells = []
+            for i in range(connectivity): #slice intersects with the object for the first time
+                current_cells.append(current_cell)
+                current_cell += 1 # we are creating different cells on the same column which are seperated by the objects
+        elif connectivity == 0:
+            current_cells = []
+            continue
+        else:
+            adj_matrix = get_adjacency_matrix(last_connectivity_parts, connective_parts)
+            new_cells = [0] * len(connective_parts)
+            for i in range(adj_matrix.shape[0]):
+                if np.sum(adj_matrix[i, :]) == 1:
+                    new_cells[np.argwhere(adj_matrix[i, :])[0][0]] = current_cells[i]
+                # If a previous part is connected to multiple parts this time, it means that IN has occurred.
+                elif np.sum(adj_matrix[i, :]) > 1: #left slice is connected to more than one part of right slice
+                    for idx in np.argwhere(adj_matrix[i, :]):
+                        new_cells[idx[0]] = current_cell
+                        current_cell = current_cell + 1
+            for i in range(adj_matrix.shape[1]):
+                # If a part of this time is connected to the last multiple parts, it means that OUT has occurred.
+                if np.sum(adj_matrix[:, i]) > 1: #right slice is connected to more than one part of left slice
+                    new_cells[i] = current_cell
+                    current_cell = current_cell + 1
+                # If this part of the part does not communicate with any part of the last time, it means that it happened in
+                elif np.sum(adj_matrix[:, i]) == 0:
+                    new_cells[i] = current_cell
+                    current_cell = current_cell + 1
+            current_cells = new_cells
+        # Draw the partition information on the map.
+        for cell, slice in zip(current_cells, connective_parts):
+            separate_img[slice[0]:slice[1], col] = cell
+        last_connectivity = connectivity
+        last_connectivity_parts = connective_parts
+        if len(current_cells) == 1: #no object in this cell
+            cell_index = current_cell -1  # cell index starts from 1
+            cell_boundaries.setdefault(cell_index,[])
+            cell_boundaries[cell_index].append(connective_parts)
+        elif len(current_cells) > 1: #cells separated by the object
+            # cells separated by the objects are not neighbor to each other
+            non_neighboor_cells.append(current_cells)
+            # non_neighboor_cells will contain many duplicate values, but we
+            # will get rid of duplicates at the end in this logic, all other cells must be neighboor to each other
+            # if their cell number are adjacent to each other like cell1 is neighboor to cell2
+            for i in range(len(current_cells)):
+                cell_index = current_cells[i]
+                # connective_parts and current_cells contain more than one
+                # cell info which are separated by the object, so we are iterating
+                # with the for loop to reach all the cells
+                cell_boundaries.setdefault(cell_index,[])
+                cell_boundaries[cell_index].append(connective_parts[i])
+    # Cell 1 is the left most cell and cell n is the right most cell, where n is the total cell number
+    all_cell_numbers = cell_boundaries.keys()
+    non_neighboor_cells = remove_duplicates(non_neighboor_cells)
+    return separate_img, current_cell, list(all_cell_numbers), cell_boundaries, non_neighboor_cells
+
+#-------------------------------------
+def display_separate_map(separate_map, cells):
+    display_img = np.empty([*separate_map.shape, 3], dtype=np.uint8)
+    random_colors = np.random.randint(0, 255, [cells, 3])
+    for cell_id in range(1, cells):
+        display_img[separate_map == cell_id, :] = random_colors[cell_id, :]
+    fig_new = plt.figure()
+    plt.imshow(display_img)
+
 """
     Input: cells_to_visit --> the order of cells to visit
     Input: cell_boundaries --> y-coordinates of each cell
         x-coordinates will be calculated in this function based on cell number
-        since first cell starts from the left and moves towards right
+        since 1st cell starts from the left and moves towards right
     Input: Nonneighbors --> This shows cells which are separated by the objects, so these cells should have the same x-coordinates.
 """
 def calculate_x_coordinates(x_size, y_size, cells_to_visit, cell_boundaries, nonneighbors):
@@ -44,23 +179,22 @@ def calculate_x_coordinates(x_size, y_size, cells_to_visit, cell_boundaries, non
         cells_x_coordinates[cell_idx] = list(range(width_accum_prev, width_current_cell+width_accum_prev))
         width_accum_prev += width_current_cell
         cell_idx = cell_idx + 1
-    return cells_x_coordinates # Output: cells_x_coordinates --> x-coordinates of each cell
+    return cells_x_coordinates # Output: x-coordinates of each cell
 
 #-----------------------------------------------
 if __name__ == '__main__':
-    # image: Export as -> Check Transparent Background and Selection Only -> Export
-    original_map = bcd.cv2.imread("files/new_map.png")
+    original_map = cv2.imread("files/new_map.png")
 
-    # We need binary image - 1's represents free space while 0's represents objects/walls
+    # We need a binary image - 1 represents free space, 0 represents objects/walls
     if len(original_map.shape) > 2:
         print("Map image is converted to binary")
         single_channel_map = original_map[:, :, 0]
-        _, binary_map = bcd.cv2.threshold(single_channel_map, 127, 1, bcd.cv2.THRESH_BINARY)
+        _, binary_map = cv2.threshold(single_channel_map, 127, 1, cv2.THRESH_BINARY)
 
-    # Call The Boustrophedon Cellular Decomposition function
-    bcd_out_im, bcd_out_cells, cell_numbers, cell_boundaries, non_neighboor_cell_numbers = bcd.bcd(binary_map)
+    # Call Boustrophedon Cellular Decomposition function
+    bcd_out_im, bcd_out_cells, cell_numbers, cell_boundaries, non_neighboor_cell_numbers = bcd(binary_map)
     # Show the decomposed cells on top of original map
-    bcd.display_separate_map(bcd_out_im, bcd_out_cells)
+    display_separate_map(bcd_out_im, bcd_out_cells)
     plt.show(block=False)
 
     # Add cost using distance between center of mass of cells
@@ -111,7 +245,7 @@ if __name__ == '__main__':
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fields)
         for i in range(len(x_coordinates)):
-            # data rows of csv file   # first, middle and last y_coordinates are special (4D)
+            # data rows of csv file   # first, middle and last y_coordinates are 4D
             cell_idx = i+1
             if ( (cell_idx == 1) | (cell_idx == len(x_coordinates)) | (cell_idx == ((len(x_coordinates)+1)/2)) ):
                 rows = [[   cell_numbers[i],
